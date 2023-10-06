7885f09ca2fea02fcd4f6e28312f67586e3af7c5
Muthulakshmi R <124235201+Muthulakshmi-R@users.noreply.github.com>
2023-10-06 12:53:50 +0530

    Add files via upload

---
+++ PasswordInputAndDisplay.java
@@ -0,0 +1,19 @@
+/**  A Java program to input and display your password */
+
+import java.io.Console;
+
+public class PasswordInputAndDisplay {
+    public static void main(String[] args) {
+        Console console = System.console();
+        
+        if (console == null) {
+            System.out.println("No console available");
+            System.exit(1);
+        }
+        
+        char[] passwordArray = console.readPassword("Input your Password: ");
+        String password = new String(passwordArray);
+        
+        System.out.println("Your password was: " + password);
+    }
+}
