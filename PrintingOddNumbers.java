32e9c5ad047d16a9ad2c576e943b5d1a2fd94190
Muthulakshmi R <124235201+Muthulakshmi-R@users.noreply.github.com>
2023-10-07 16:19:26 +0530

    Add files via upload

---
+++ PrintingOddNumbers.java
@@ -0,0 +1,11 @@
+/** Java program to print odd numbers from 1 to 99 - one per line */
+
+public class PrintingOddNumbers {
+	public static void main(String[] args) {
+				for (int i = 1; i <= 99; i++) {
+            				if (i % 2 != 0) {
+                				System.out.println(i);
+            				}
+        			}
+    	}
+}
